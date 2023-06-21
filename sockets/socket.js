ad61f1d5bd72043b9c692951d5a896bf9ec62df5
jesus3c12 <jesus3c12@gmail.com>
2023-06-20 20:23:51 -0700

    Fin de la app - Backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,37 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require ('../controllers/socket');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    const [ valido, uid ] = comprobarJWT( client.handshake.headers['x-token'] );
 
+    // Verificar autenticacion
+
+    if ( !valido ) { return client.disconnect(); }
+
+    // Cliente autenticado
+    usuarioConectado( uid );
+
+    // Ingresar al usuario en una sala en especifico
+    // Sala global, client.id
+    client.join( uid );
+
+    // Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async( payload ) => {
+        // TODO: guardar mensaje
+
+        await grabarMensaje( payload );
+
+        io.to( payload.para ).emit('mensaje-personal', payload );
+    })
+    
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado( uid );
+        //console.log('Cliente desconectado');
     });
-
     //client.on('mensaje', ( payload ) => {
     //    console.log('Mensaje', payload);
     //    io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
